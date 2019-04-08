71f9f3d2433f35529553f7f321797fb6a57ff7f0
Sergey Bondarev <bsa0393@gmail.com>
2019-04-08 22:30:26 +0300

    fix corner case

--- problems/4 - missing-integer/naive_solution.py
+++ problems/4 - missing-integer/naive_solution.py
@@ -1,5 +1,9 @@
 # it's not a linear time solution
 def lowest_positive(arr):
+	max_ = max(arr)
+	if max_ <= 0:
+		return 1
+
 	min_ = min([i for i in arr if i > 0]) - 1
 
 	if min_ > 1:
@@ -13,6 +17,7 @@ def lowest_positive(arr):
 
 assert lowest_positive([3, 4, -1, 1]) == 2
 assert lowest_positive([1, 2, 0]) == 3
+assert lowest_positive([-1,-2,-3]) == 1
 
 
 
