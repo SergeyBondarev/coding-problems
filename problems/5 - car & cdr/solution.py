f806e4f69dc0c9e7b8bbb32414971c443b977e18
Sergey Bondarev <bsa0393@gmail.com>
2019-11-01 22:17:47 +0300

    Add problem #5 with python solution

---
+++ problems/5 - car & cdr/solution.py
@@ -0,0 +1,16 @@
+def cons(a, b):
+    def pair(f):
+    	return f(a, b)
+    return pair
+
+
+def car(cons):
+	return cons(lambda x,y: x)
+
+
+def cdr(cons):
+	return cons(lambda x,y: y)
+
+
+assert car(cons(3,4)) == 3
+assert cdr(cons(3,4)) == 4
